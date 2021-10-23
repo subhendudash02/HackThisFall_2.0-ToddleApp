e523e67d70596805ca874cb7b1a023e2164f07c7
Shruthi-S1302 <89682079+Shruthi-S1302@users.noreply.github.com>
2021-10-23 21:18:39 +0530

    Instagram

---
+++ insta_list.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ig_clone1/insta_stories.dart';
+
+class InstaList extends StatefulWidget {
+  @override
+  _InstaListState createState() => _InstaListState();
+}
+
+class _InstaListState extends State<InstaList> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => index == 0
+          ? new SizedBox(
+              child: new InstaStories(),
+              height: deviceSize.height * 0.15,
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          new Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1422132473924784135/V_QWeu3Q_400x400.jpg")),
+                            ),
+                          ),
+                          new SizedBox(
+                            width: 10.0,
+                          ),
+                          new Text(
+                            "Shruthi",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      new IconButton(
+                        icon: Icon(Icons.more_vert),
+                        onPressed: null,
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: new Image.network(
+                    "https://images.squarespace-cdn.com/content/v1/5807a5666b8f5ba082451a2f/1496083276042-NOX6OIOAV0SZSNQ0HZUC/Lounge+Books+-+Bookstagrammer+-+The+Halcyon+Days+of+Summer",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new IconButton(
+                            icon: new Icon(isPressed
+                                ? Icons.favorite
+                                : FontAwesomeIcons.heart),
+                            color: isPressed ? Colors.red : Colors.black,
+                            onPressed: () {
+                              setState(() {
+                                isPressed = !isPressed;
+                              });
+                            },
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(FontAwesomeIcons.bookmark)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Liked by Shreya, pk and 528,331 others",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        height: 40.0,
+                        width: 40.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://pbs.twimg.com/profile_images/1422132473924784135/V_QWeu3Q_400x400.jpg")),
+                        ),
+                      ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: new TextField(
+                          decoration: new InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Add a comment...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child:
+                      Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
+                )
+              ],
+            ),
+    );
+  }
+}
